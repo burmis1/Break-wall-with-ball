51ab7836bce9b706c05529ffb077ea5871497955
burmis1 <malteburmeister@hotmail.com>
2024-04-04 08:30:52 +0200

    hej

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+array1
+array2
+array3
+array4
+array5
+array6
+
+
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  circle(200,60,30)
+  rect(30, 20, 55, 55)
 }
